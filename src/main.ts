af3da739e341285c5f1379b17c3dc104b96d9b53
Cameron Coleman <142551249+TheSkyIsForward2@users.noreply.github.com>
2025-11-12 23:07:27 +0000

    Added Map and starting marker

---
+++ src/main.ts
@@ -0,0 +1,60 @@
+// @deno-types="npm:@types/leaflet"
+import leaflet from "leaflet";
+
+// Style sheets
+import "leaflet/dist/leaflet.css"; // supporting style for Leaflet
+import "./style.css";
+
+// Fix missing marker images
+import "./_leafletWorkaround.ts";
+
+// Import our luck function
+
+// Create basic UI elements
+
+const controlPanelDiv = document.createElement("div");
+controlPanelDiv.id = "controlPanel";
+document.body.append(controlPanelDiv);
+
+const mapDiv = document.createElement("div");
+mapDiv.id = "map";
+document.body.append(mapDiv);
+
+const inventoryDiv = document.createElement("div");
+inventoryDiv.id = "inventory";
+document.body.append(inventoryDiv);
+
+// Our classroom location
+const CLASSROOM_LATLNG = leaflet.latLng(
+  36.997936938057016,
+  -122.05703507501151,
+);
+
+// Tunable gameplay parameters
+const GAMEPLAY_ZOOM_LEVEL = 19;
+const GAMEPLAY_ZOOM_MAX = 1;
+const TILE_DEGREES = 1e-4;
+const RANGE = 8;
+const CACHE_SPAWN_PROBABILITY = 0.1;
+
+// Create the map (element with id "map" is defined in index.html)
+const map = leaflet.map(mapDiv, {
+  center: CLASSROOM_LATLNG,
+  zoom: GAMEPLAY_ZOOM_LEVEL,
+  minZoom: GAMEPLAY_ZOOM_MAX,
+  maxZoom: GAMEPLAY_ZOOM_LEVEL,
+  zoomControl: false,
+  scrollWheelZoom: false,
+});
+
+// Populate the map with a background tile layer
+leaflet
+  .tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  })
+  .addTo(map);
+
+const center = leaflet.marker(CLASSROOM_LATLNG);
+center.addTo(map);
